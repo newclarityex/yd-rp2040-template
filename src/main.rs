a5a9c7a3791b933167cc3398ef156445f3779b54
newclarityex <69740123+newclarityex@users.noreply.github.com>
2025-03-13 02:09:38 -0400

    simple template

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+#![no_std]
+#![no_main]
+
+use embedded_hal::digital::OutputPin;
+use panic_halt as _;
+use vcc_gnd_yd_rp2040::entry;
+use vcc_gnd_yd_rp2040::{
+    Pins, XOSC_CRYSTAL_FREQ,
+    hal::{
+        Sio,
+        clocks::{Clock, init_clocks_and_plls},
+        pac,
+        watchdog::Watchdog,
+    },
+};
+
+#[entry]
+fn main() -> ! {
+    let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+
+    let mut watchdog = Watchdog::new(pac.WATCHDOG);
+
+    let clocks = init_clocks_and_plls(
+        XOSC_CRYSTAL_FREQ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+
+    let sio = Sio::new(pac.SIO);
+    let pins = Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+    let mut led_pin = pins.led.into_push_pull_output();
+
+    loop {
+        led_pin.set_high().unwrap();
+        delay.delay_ms(500);
+        led_pin.set_low().unwrap();
+        delay.delay_ms(500);
+    }
+}
